2822c1ce712bcd254a3829decf80595f63be2ff6
Aliaksandr Kananenka <Aliaksandr_Kananenka@epam.com>
2018-07-20 00:29:42 +0300

    using userform

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
 import React, {Component} from 'react'
 import ArticleList from './ArticleList'
 import ArticleChart from './ArticlesChart'
+import UserForm from './UserForm'
 
 //здесь мы работаем со сторонней библиотекой как с обычным компонентом
 class App extends Component {
-
+    
     render () {
         return (
             <div>
+                <UserForm/>
                 <ArticleList articles = {this.props.articles}/>
                 <ArticleChart articles = {this.props.articles}/>
             </div>
