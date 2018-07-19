e1677e4e10e98f2b756e29b71d2f13368f850a54
Aliaksandr Kananenka <Aliaksandr_Kananenka@epam.com>
2018-07-20 01:03:57 +0300

    simple use external component (react-select)

--- src/components/App.js
+++ src/components/App.js
@@ -2,19 +2,31 @@ import React, {Component} from 'react'
 import ArticleList from './ArticleList'
 import ArticleChart from './ArticlesChart'
 import UserForm from './UserForm'
-
+import Select from 'react-select'
 //здесь мы работаем со сторонней библиотекой как с обычным компонентом
 class App extends Component {
+
+    state = {
+        selection: null
+    }
     
     render () {
+        const options = this.props.articles.map(article => ({
+            label: article.title,
+            value: article.id
+        }))
+
         return (
             <div>
                 <UserForm/>
+                <Select options = {options} value = {this.state.selection} onChange = {this.changeSelection}/>
                 <ArticleList articles = {this.props.articles}/>
                 <ArticleChart articles = {this.props.articles}/>
             </div>
         )
     }
+
+    changeSelection = selection => this.setState({selection})
 }
 
 export default App
